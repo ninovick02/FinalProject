6c79f30edc52456b51dc6a9352fa4cc3a45f9b51
Naomi Novick <naomi.i.novick@gmail.com>
2025-12-03 17:31:27 -0500

    Api file

---
+++ API/plumber.R
@@ -0,0 +1,40 @@
+library(plumber)
+library(tidyverse)
+
+# Loading saved fits and workflows
+load("savedrf.RData")
+load("savedtree.Rdata")
+
+#* @apiTitle Diabetes Fits
+#* @apiDescription Plumber example description.
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg = "") {
+    list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+function() {
+    rand <- rnorm(100)
+    hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+    as.numeric(a) + as.numeric(b)
+}
+
+# Programmatically alter your API
+#* @plumber
+function(pr) {
+    pr %>%
+        # Overwrite the default serializer to return unboxed JSON
+        pr_set_serializer(serializer_unboxed_json())
+}
